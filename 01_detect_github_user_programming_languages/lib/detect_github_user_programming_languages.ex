efe8104460156a278bce86189657c02b6239f405
Alexey Vasilyev <bestspajic@gmail.com>
2020-01-15 23:59:53 +0300

    Init projects structure with mix new

---
+++ 01_detect_github_user_programming_languages/lib/detect_github_user_programming_languages.ex
@@ -0,0 +1,18 @@
+defmodule DetectGithubUserProgrammingLanguages do
+  @moduledoc """
+  Documentation for DetectGithubUserProgrammingLanguages.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DetectGithubUserProgrammingLanguages.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 01_detect_github_user_programming_languages/test/detect_github_user_programming_languages_test.exs
@@ -0,0 +1,8 @@
+defmodule DetectGithubUserProgrammingLanguagesTest do
+  use ExUnit.Case
+  doctest DetectGithubUserProgrammingLanguages
+
+  test "greets the world" do
+    assert DetectGithubUserProgrammingLanguages.hello() == :world
+  end
+end
